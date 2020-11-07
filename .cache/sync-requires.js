aa93729d7efba26e2ab2403939b68c9ce92db291
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-11-08 00:11:27 +0500

    update

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("N:\\BOOTCAMP 8 TO 12\\CRUD APP FAUNADB\\Serverless-CRUD-master\\Serverless-CRUD-master\\.cache\\dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("N:\\BOOTCAMP 8 TO 12\\CRUD APP FAUNADB\\Serverless-CRUD-master\\Serverless-CRUD-master\\src\\pages\\index.js")))
 }
 
